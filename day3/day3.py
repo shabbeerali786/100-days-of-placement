78b95730e4c7cfd173f27d9c452ab66110da8230
shabbeerali786 <shabbeeralij.23it@kongu.edu>
2026-08-01 09:48:10 +0530

    day3_binary_search

--- day3/day3.py
+++ day3/day3.py
@@ -1,3 +1,5 @@
+// search the correct index of an element in an array
+// using linear search
 n = int(input("Enter the number of elements: "))
 
 arr = list(map(int, input("Enter the elements: ").split()))
@@ -17,3 +19,39 @@ else:
 
     if not found:
         print("Element not found")
+        
+        
+        
+
+
+// search the correct index of an element in an array
+// using binary search
+
+
+n = int(input("Enter the number of element: "))
+arr = list(map(int, input("Enter the element: ").split()))
+if len(arr) != n:
+    print("enter number of correct element: ")
+else:
+    key = int(input("Enter searching element: "))
+    
+    low = 0
+    high = n-1
+    
+    found = False
+    while low <= high:
+        mid = (low + high) // 2
+        
+        if arr[mid] == key
+            print("seaching element at index: ", mid)
+            found = True
+            break
+        elif key < arr[mid]:
+            high = mid - 1
+        else:
+            low = mid + 1
+    
+    if not found:
+        print(" searching element")
+            
+    
