4957ecca0cd0a1b07e2052aba339d641dfdc46e8
shabbeerali786 <shabbeeralij.23it@kongu.edu>
2026-08-06 14:44:28 +0530

    day4_2

--- day4/day4.py
+++ day4/day4.py
@@ -6,3 +6,20 @@ if sort1 == sort2:
     print("Anagram")
 else:
     print("Not Anagram")
+    
+    
+    
+////////////////////////////////////////
+
+n1 = int(input())
+l1 = list(map(int, input().split()))
+n2 = int(input())
+l2 = list(map(int, input().split()))
+mod = 1000000007
+num1 = 0
+for d in l1:
+    num1 = (num1*10+d)%mod
+num2 = 0
+for d in l2:
+    num2 = (num2*10+d)%mod
+print((num1*num2)%mod)
