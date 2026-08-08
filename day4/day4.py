cda5185974d6f152b21dd6982d274d5a27cb2cff
shabbeerali786 <shabbeeralij.23it@kongu.edu>
2026-08-08 09:59:13 +0530

    traped water

--- day4/day4.py
+++ day4/day4.py
@@ -23,3 +23,36 @@ num2 = 0
 for d in l2:
     num2 = (num2*10+d)%mod
 print((num1*num2)%mod)
+
+
+
+////////////////////////////////
+
+
+def trap(height):
+    left = 0
+    right = len(height) - 1
+    
+    left_max = 0
+    right_max = 0
+    water = 0
+    
+    while left < right:
+        if height[left] < height[right]:
+            if height[left] >= left_max:
+                left_max = height[left]
+            else:
+                water += left_max - height[left]
+            left += 1
+            
+        else:
+            if height[right] >= right_max:
+                right_max = height[right]
+            else:
+                water += left_right - height[right]
+            right -= 1
+            
+    return water
+    
+height = list(map(int, input().split()))
+print("trapped water", trap(height))
