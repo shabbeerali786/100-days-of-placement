d4a8fbd56bc83a358a6b40639ac59e9925ae2ce6
shabbeerali786 <shabbeeralij.23it@kongu.edu>
2026-08-19 16:12:50 +0530

    factorial

--- day4/day4.py
+++ day4/day4.py
@@ -275,3 +275,14 @@ if prime:
     print("prime")
 else:
     print("Not prime")    
+    
+    
+
+
+18.# factorial of a number
+
+n = int(input("Enter a number"))
+factorial = 1
+for i in range(1, n+1):
+    factorial = factorial * i
+print("factorial",factorial)
