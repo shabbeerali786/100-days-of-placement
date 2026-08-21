6aab8ebc21ff11d983938fe7a09d7bbff9453d1b
shabbeerali786 <shabbeeralij.23it@kongu.edu>
2026-08-21 09:32:19 +0530

    sum of digit

--- day4/day4.py
+++ day4/day4.py
@@ -286,3 +286,14 @@ factorial = 1
 for i in range(1, n+1):
     factorial = factorial * i
 print("factorial",factorial)
+
+
+19. # sum of digits of a number
+
+n = int(input("Enter the number"))
+sum =0
+while n>0:
+    digit = n%10
+    sum = sum+digit
+    n = n//10
+print(sum)
